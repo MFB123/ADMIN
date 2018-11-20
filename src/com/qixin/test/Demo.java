3f17c6dad25625a034c7cec93989954fa56b548e
xGF <910913082@qq.com>
2018-11-20 16:05:14 +0800

    第一次提交demo到mester，刚刚那个不算

--- src/com/qixin/test/Demo.java
+++ src/com/qixin/test/Demo.java
@@ -1,5 +1,12 @@
 package com.qixin.test;
 
 public class Demo {
+	
+	 public static void demo1() {  // 第一次提交测试
+		 System.out.println("测试1111");
+		 System.out.println("测试1111");
+		 System.out.println("测试1111");
+	 }
+	
 
 }
